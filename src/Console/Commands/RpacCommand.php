3a6ed866f29db8f58a307d3d90f8c9d7602ab30b
pm <pm@101media.ru>
2020-03-25 21:07:08 +0300

    Console up

--- src/Console/Commands/RpacCommand.php
+++ src/Console/Commands/RpacCommand.php
@@ -2,6 +2,7 @@
 
 namespace Codewiser\Rpac\Console\Commands;
 
+use Codewiser\Rpac\Role;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 
@@ -21,7 +22,6 @@ class RpacCommand extends Command
      */
     protected $description = 'Создаёт роль и привязывает/создаёт пользователя';
 
-    protected $roleModel;
     protected $userModel;
 
     /**
@@ -32,7 +32,6 @@ class RpacCommand extends Command
     public function __construct()
     {
         $this->userModel = config('rpac.models.user');
-        $this->roleModel = config('rpac.models.role');
 
         parent::__construct();
     }
@@ -47,54 +46,48 @@ class RpacCommand extends Command
         $acts = explode(':', $this->argument('act'));
 
 
-        if(!count($acts)) {
-            $info = 'Непонятный аргумент ('. $this->argument('act') .'). Нужно так: `php artisan rpac su:1` или `php artisan rpac admin:email@example.com:password`';
-        }
-        else {
+        if (!count($acts)) {
+            $info = 'Непонятный аргумент (' . $this->argument('act') . '). Нужно так: `php artisan rpac su:1` или `php artisan rpac admin:email@example.com:password`';
+        } else {
 
             $roleSlug = array_shift($acts);
             $userSlug = array_shift($acts);
             $userPass = array_shift($acts);
 
-            if(!$userSlug) {
+            if (!$userSlug) {
                 $info = 'Нужно указать EMAIL или ID. Например: `php artisan rpac su:1` или `php artisan rpac admin:email@example.com:password`';
-            }
-            else {
+            } else {
                 $error = false;
                 $user = null;
                 $userEmail = filter_var($userSlug, FILTER_VALIDATE_EMAIL);
 
 
-                if($userEmail) {
+                if ($userEmail) {
                     $user = $this->userModel::where('email', $userEmail)->first();
-                }
-                else {
+                } else {
                     $user = $this->userModel::find($userSlug);
-                    if(!$user) {
+                    if (!$user) {
                         $error = 'Не найден пользователь по ID (или неверный формат EMAIL). Попробуйте указать EMAIL: `php artisan rpac su:email@example.com`';
                     }
                 }
 
-                if($error) {
+                if ($error) {
                     $info = $error;
-                }
-                else {
+                } else {
 
-                    $role = $this->roleModel::firstOrCreate(['slug' => $roleSlug]);
-                    if(!$role->name) $role->update(['name' => $role->slug]);
+                    $role = Role::query()->firstOrCreate(['id' => $roleSlug]);
 
-                    if(!$user) {
+                    if (!$user) {
                         // Если не указан пароль - генерируем
-                        if(!$userPass) $userPass = Str::random(6);
+                        if (!$userPass) $userPass = Str::random(6);
 
                         $user = $this->userModel::create(['name' => $userEmail, 'email' => $userEmail, 'password' => bcrypt($userPass), 'api_token' => Str::random(60)]);
                         $info = 'Cоздан';
-                    }
-                    else {
+                    } else {
                         $info = 'Найден';
 
                         // Если указан пароль - шифруем и сохраняем
-                        if($userPass) {
+                        if ($userPass) {
                             $user->password = bcrypt($userPass);
                             $user->save();
                         }
@@ -102,24 +95,22 @@ class RpacCommand extends Command
                     }
 
                     $info .= ' пользователь с ID: [' . $user->id . '] и EMAIL: [' . $user->email . ']. ';
-                    if($userPass) {
+                    if ($userPass) {
                         $info .= ' Пароль пользователя: [' . $userPass . ']. ';
                     }
 
-                    if(!$user->roles) {
+                    if (!$user->roles) {
                         $info = 'Модель User должна использовать трейт Rpacable (или Wfacable) и иметь protected $with = [roles]';
-                    }
-                    else {
+                    } else {
                         // Если нет токена - создаём
-                        if(!$user->api_token) {
+                        if (!$user->api_token) {
                             $user->api_token = Str::random(60);
                             $user->save();
                         }
 
-                        if($user->roles->contains('id', $role->id)) {
+                        if ($user->roles->contains('id', $role->id)) {
                             $info .= 'Роль [' . $role->slug . '] привязана ранее';
-                        }
-                        else {
+                        } else {
                             $user->roles()->attach($role->id);
                             $info .= 'Роль [' . $role->slug . '] привязана к пользователю';
                         }
