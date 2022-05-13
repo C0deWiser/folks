3e215fd8f77e904b7b816335a39787d6d7c445c5
Михаил Погребников <pm@101media.ru>
2022-05-13 18:52:54 +0300

    color and icons

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -24,9 +24,8 @@
 
     <div class="container mb-5">
         <div class="d-flex align-items-center py-4 header">
-            <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-translate logo" viewBox="0 0 16 16">
-                <path class="fill-primary" d="M4.545 6.714 4.11 8H3l1.862-5h1.284L8 8H6.833l-.435-1.286H4.545zm1.634-.736L5.5 3.956h-.049l-.679 2.022H6.18z"/>
-                <path class="fill-primary" d="M0 2a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v3h3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-3H2a2 2 0 0 1-2-2V2zm2-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H2zm7.138 9.995c.193.301.402.583.63.846-.748.575-1.673 1.001-2.768 1.292.178.217.451.635.555.867 1.125-.359 2.08-.844 2.886-1.494.777.665 1.739 1.165 2.93 1.472.133-.254.414-.673.629-.89-1.125-.253-2.057-.694-2.82-1.284.681-.747 1.222-1.651 1.621-2.757H14V8h-3v1.047h.765c-.318.844-.74 1.546-1.272 2.13a6.066 6.066 0 0 1-.415-.492 1.988 1.988 0 0 1-.94.31z"/>
+            <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-translate logo" viewBox="0 0 24 24">
+                <path class="fill-primary" d="M10 2h4v4h-4V2zM3 7h18v2h-6v13h-2v-6h-2v6H9V9H3V7z"/>
             </svg>
 
             <h4 class="mb-0 ml-2">
@@ -47,8 +46,10 @@
                     </li>
                     <li class="nav-item">
                         <router-link active-class="active" to="/users" class="nav-link d-flex align-items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box" viewBox="0 0 16 16">
-                                <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5 8 5.961 14.154 3.5 8.186 1.113zM15 4.239l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box" viewBox="0 0 128 128">
+                                <circle id="_x32_" cx="64.5" cy="28" r="21"/>
+                                <path d="M30.4,118.2V85.7c0-1.6,1-2.6,2.6-2.6c1.6,0,2.6,1,2.6,2.6v32.5h57.7V85.7c0-1.6,1-2.6,2.6-2.6c1.6,0,2.6,1,2.6,2.6v32.5
+                                h18.4V80.4c0-14.4-11.8-26.2-26.2-26.2H38.3c-14.4,0-26.2,11.8-26.2,26.2v37.8H30.4z"/>
                             </svg>
                             <span>Users</span>
                         </router-link>
